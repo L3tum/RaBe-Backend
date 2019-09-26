7849b0270194f072c9b23b065455d8faa0c9bb53
PaulyT <tom.pauly@cbc.de>
2019-09-26 08:48:28 +0200

    Update

--- RaBe/Controllers/CategoriesController.cs
+++ RaBe/Controllers/CategoriesController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using RaBe;
+using RaBe.Model;
 
 namespace RaBe.Controllers
 {
