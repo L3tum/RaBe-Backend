4b57469adde987dca4bda20aaf215f0c5219f09c
L3tum <9307432+L3tum@users.noreply.github.com>
2019-09-30 16:30:20 +0200

    Fix backend

---
+++ RaBe/ResponseModel/LoginResponse.cs
@@ -0,0 +1,34 @@
+﻿#region using
+
+using RaBe.Model;
+
+#endregion
+
+namespace RaBe.ResponseModel
+{
+	public class LoginResponse
+	{
+		public bool administrator;
+		public bool blocked;
+		public string email;
+		public string name;
+		public bool passwordGeaendert;
+		public string token;
+
+		public static LoginResponse FromTeacher(Lehrer teacher)
+		{
+			var response = new LoginResponse
+			{
+				administrator = teacher.Administrator,
+				blocked = teacher.Blocked,
+				email = teacher.Email,
+				name = teacher.Name,
+				passwordGeaendert = teacher.PasswordGeaendert,
+				token = teacher.Token
+			};
+
+
+			return response;
+		}
+	}
+}
